d29ddf6e30acc43de5d708b837ad2f35bb12ebff
Jesus Manuel Garnica Bonome <j.garnica@lab.matcom.uh.cu>
2016-12-05 08:35:03 -0800

    a skeleton of the project

---
+++ Hive/drone/Volatile/TestTask/vector_mult.py
@@ -0,0 +1,51 @@
+﻿import tasksupport
+import asyncio
+import Volatile.TestTask.a
+
+def vector_serializer(vector : (int, list, list)) -> str:
+    return "{0}|{1}|{2}".format(vector[0],
+                                " ".join(vector[1]),
+                                " ".join(vector[2]))
+
+def vector_deserializer(vector : str) -> (int, list, list):
+    struct = vector.split('|')
+    return (int(struct[0]),
+           [int(i) for i in vector[1].split()],
+           [int(i) for i in vector[2].split()])
+
+split_threshold, vector_a, vector_b = vector_deserializer(tasksupport.CurrentTask.data)
+
+if len(vector_a) != len(vector_b):
+    raise Exception("vectors length mismatch")
+
+@asyncio.coroutine
+def vector_mult(v_a : list, v_b : list) -> list:
+    if len(v_a) > split_threshold:
+        name = tasksupport.CurrentTask.name
+        split = len(v_a)//2
+
+        data = vector_serializer(split_threshold, v_a[:split], v_b[:split])
+        task1 = tasksupport.distributed_process(name, data)
+
+        data = vector_serializer(split_threshold, v_a[split:], v_b[split:])
+        task2 = tasksupport.distributed_process(name, data)
+
+        return (yield from task1) + (yield from task2)
+
+    return [v_a[i]*v_b[i] for i in range(len(v_a))]
+
+@asyncio.coroutine
+def task():
+    tasksupport.CurrentTask.finalize((yield from vector_mult(vector_a, vector_b)))
+    print("finalized task: ", tasksupport.CurrentTask.task_id)
+
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(task())
+
+except:
+    print("!error task: ",tasksupport.CurrentTask.task_id)
+
+finally:
+    loop.close()
---
+++ Hive/drone/tasksupport.py
@@ -0,0 +1,58 @@
+﻿from asyncio import coroutine
+
+class Task:
+
+    def __init__(self, name : str, data : str, *, owneraddr : (str, int), taskid : int):
+        self._name = name
+        self._data = data
+        self._owneraddr = owneraddr
+        self._taskid = taskid
+        self._result = None
+        self._done = False
+
+    @property
+    def task_id(self):
+        return self._taskid
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def data(self):
+        return self._data
+    
+    @property
+    def result(self) -> str:
+        return self._result
+
+    @result.setter
+    def set_result(self, result : str) -> None:
+        self._result = result
+
+    @property
+    def done(self):
+        return self._done;
+    @property
+    def owner(self):
+        return self._owneraddr
+
+    def finalize(self, result):
+        self.result = result
+        self.done = True
+
+#meter esto en local
+CurrentTask = None
+_drone = None
+
+@coroutine
+def distributed_process(task : str, data : str,  dependencies : list = None):
+    if _drone == None:
+        raise Exception("no drone attached")
+    task = _drone.addtask(task, data, dependencies)
+    
+    if task == None:
+        raise Exception("could not start the task")
+    
+
+    return (yield from _drone.await_task(task))
